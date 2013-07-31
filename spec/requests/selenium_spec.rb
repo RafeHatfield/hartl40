421f341ed4bb382847c7a22d9fc6294325a2fdd5
Patrick Audley <paudley@blackcat.ca>
2013-07-31 16:41:06 -0700

    Update selenium_spec.rb
    Fix failing test.

--- spec/requests/selenium_spec.rb
+++ spec/requests/selenium_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe 'home page' do
   it 'welcomes the user' do
     visit 'http://google.com'
-    page.should have_content('Welcome')
+    page.should have_content('Google')
   end
 end
