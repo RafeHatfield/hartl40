f05cd3706d3d3f5c458099f5318e98a3564c5629
RafeHatfield <rafe.hatfield@suite101.com>
2013-08-02 10:51:32 -0700

    following complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Hartl40::Application.routes.draw do
 
   root  'static_pages#home'
   
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   match '/signup',  to: 'users#new',            via: 'get'
 
   resources :sessions, only: [:new, :create, :destroy]
@@ -10,6 +14,7 @@ Hartl40::Application.routes.draw do
   match '/signout', to: 'sessions#destroy',     via: 'delete'
 
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
